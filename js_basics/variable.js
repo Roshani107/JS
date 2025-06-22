9e28544552e28f3b8ae61c375932987d1734bd88
Roshani kushwaha <roshanikswha@gmail.com>
2025-06-22 09:17:14 +0000

    Java script variables declaration

---
+++ js_basics/variable.js
@@ -0,0 +1,7 @@
+let r= "roshani"
+const g= 12345 // ye kbhi change nhi hoga 
+email = "rosani@123"
+ var pass = 123456789
+let def;
+ console.table([r,g,email,pass,def]);
+  console.log(r,g,email,pass,def);
